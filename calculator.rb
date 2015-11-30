babb410529c6ea11c0b77ca1ba3d382986d9e9ba
Vinay Dandagi <vinaydandagi@gmail.com>
2015-11-30 18:17:38 +0530

    6th commit

--- calculator.rb
+++ calculator.rb
@@ -48,4 +48,4 @@ puts "5 pressed"
 puts "EQUALS button pressed....DIFF action completed!!"
 
 driver.quit
-puts "TEST SUCCEEDED!! HOORAYAAAArrrrraaaa"
+puts "TEST SUCCEEDED!! HOORAYAAAArrrrraaaaiiiii"
